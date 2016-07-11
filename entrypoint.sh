bff5469cef0b14230228fcef414695eca8ddc374
Peter McConnell <peter.mcconnell@rehabstudio.com>
2016-07-11 16:11:50 +0100

    Added ability to use existing users

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,14 @@
 # fallback
 
 USER_ID=${LOCAL_USER_ID:-1000}
-
 echo "Starting with UID : $USER_ID"
-adduser -s /bin/shell -u $USER_ID -h /home/user -D -H user
-export HOME=/home/user
 
-mkdir -p /home/user
-chown -R user: /home/user
+if [ -z "$(getent passwd $USER_ID)" ]; then
+	adduser -s /bin/shell -u $USER_ID -h /home/user -D -H user
+	export HOME=/home/user
 
-exec /usr/bin/gosu user "$@"
+	mkdir -p /home/user
+	chown -R user: /home/user
+fi
+
+exec /usr/bin/gosu "$(getent passwd "${USER_ID}" | cut -d: -f1)" "$@"
